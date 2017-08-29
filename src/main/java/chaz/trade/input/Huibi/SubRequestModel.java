25ad76c942ce6a0eb703b6acfefd1bbae66cdbec
chazwong <chazwong@gmail.com>
2017-08-29 23:08:54 +0800

    update

---
+++ src/main/java/chaz/trade/input/Huibi/SubRequestModel.java
@@ -0,0 +1,25 @@
+package chaz.trade.input.Huibi;
+
+/**
+ * Created by Administrator on 2017/8/29.
+ */
+public class SubRequestModel {
+    private String sub;
+    private String id;
+
+    public String getSub() {
+        return sub;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
